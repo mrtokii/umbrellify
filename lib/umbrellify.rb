c708846c3b330dc7c8a4d45529bf822d26396201
mrtokii <mrtokii@outlook.com>
2020-03-25 23:07:08 +0300

    Add VS Code workspace and build task

--- lib/umbrellify.rb
+++ lib/umbrellify.rb
@@ -1,3 +1,5 @@
-require_relative './Configuration/Configuration'
-require_relative './ImportsManager/ImportsManager'
-require_relative './ProjectManager/ProjectManager'
+module Umbrellify
+  require_relative "./Configuration/Configuration"
+  require_relative "./ImportsManager/ImportsManager"
+  require_relative "./ProjectManager/ProjectManager"
+end
